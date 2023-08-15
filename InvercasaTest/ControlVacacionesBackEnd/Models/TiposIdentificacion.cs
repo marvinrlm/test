fde21e5429042486e68c0b7b222dd15a31a04d13
marvinrlm <marvin.rlm@gmail.com>
2023-08-15 13:30:13 -0600

    RollBack
    RollBack de los 2 proyectos web

---
+++ InvercasaTest/ControlVacacionesBackEnd/Models/TiposIdentificacion.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlVacacionesBackEnd.Models
+{
+    public class TiposIdentificacion
+    {
+        public int Id { get; set; }
+        [Required]
+        public string TipoIdentificacion { get; set; }=string.Empty;
+    }
+}
